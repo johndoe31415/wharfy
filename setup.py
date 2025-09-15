4eedbacb9822a3dc85e90358af93d907c3cb886d
Johannes Bauer <joe@johannes-bauer.com>
2025-09-15 18:04:52 +0200

    Bump version
    Bump up version to v0.0.2rc0.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 setuptools.setup(
 	name = "wharfy",
 	packages = setuptools.find_packages(),
-	version = "0.0.1",
+	version = "0.0.2rc0",
 	license = "gpl-3.0",
 	description = "Tiny Docker orchestration toolkit without Yaml",
 	long_description = long_description,
@@ -14,7 +14,7 @@ setuptools.setup(
 	author = "Johannes Bauer",
 	author_email = "joe@johannes-bauer.com",
 	url = "https://github.com/johndoe31415/wharfy",
-	download_url = "https://github.com/johndoe31415/wharfy/archive/v0.0.1.tar.gz",
+	download_url = "https://github.com/johndoe31415/wharfy/archive/v0.0.2rc0.tar.gz",
 	keywords = [ "docker", "orchestration" ],
 	install_requires = [ ],
 	entry_points = {
